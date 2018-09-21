630f4f6fd42277803fe7be9e05a3b5b6280bbdd7
zteoh <zteoh@ucsd.edu>
2018-09-21 17:19:57 +0800

    removed fixed-action button display if not logged in

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Dashboard extends Component {
 	render() {
-		return (
-			<div>
-				<div className="fixed-action-btn">
-					<Link to="/surveys" className="btn-floating btn-large waves-effect waves-light red">
-						<i className="large material-icons">menu</i>
-					</Link>
+		if(this.props.auth) {
+			return (
+				<div>
+					<div className="fixed-action-btn">
+						<Link to="/surveys" className="btn-floating btn-large waves-effect waves-light red">
+							<i className="large material-icons">menu</i>
+						</Link>
 
-					<ul>
-					    <li><Link to="/surveys/new" className="btn-floating green"><i className="material-icons">add</i></Link></li>
-	 				</ul>
+						<ul>
+						    <li><Link to="/surveys/new" className="btn-floating green"><i className="material-icons">add</i></Link></li>
+		 				</ul>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
+		else {
+			return null;
+		}
 	}
 }
 
-export default Dashboard;
+function mapStateToProps(state) {
+	return {
+		auth: state.auth
+	};
+}
+
+export default connect(mapStateToProps,null)(Dashboard);
