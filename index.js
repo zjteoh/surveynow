c8d02b0715ef5c4a26cb04b907dfc5acd6f577fe
zteoh <zteoh@ucsd.edu>
2018-09-21 14:28:30 +0800

    fixed production bugs

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/client/build')));
     //No more changes from here on now
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client'
-, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 }
 
