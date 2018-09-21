e18cd27a235e7983490d21586703c85c2b38268e
zteoh <zteoh@ucsd.edu>
2018-09-21 14:32:49 +0800

    added empty surveys text

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -20,25 +20,30 @@ class SurveyList extends Component {
 	renderSurveys() {
 		if(this.props.auth) {
 			return this.props.surveys.reverse().map(survey => {
-				return (
-					<div className="card cyan lighten-5" key={survey._id}>
-						<div className="card-content">
-							<span className="card-title"><b>Title:</b> {survey.title}</span>
-							<p>
-								<b>Body:</b> {survey.body}
-							</p>
-							<p className="right">
-								<p><b>Date Sent:</b> {this.getDateString(survey.dateSent)}</p>
-								<p><b>Last Responded:</b> {this.getDateString(survey.lastResponded)}</p>
-							</p>
+				if(survey) {
+					return (
+						<div className="card cyan lighten-5" key={survey._id}>
+							<div className="card-content">
+								<span className="card-title"><b>Title:</b> {survey.title}</span>
+								<p>
+									<b>Body:</b> {survey.body}
+								</p>
+								<p className="right">
+									<p><b>Date Sent:</b> {this.getDateString(survey.dateSent)}</p>
+									<p><b>Last Responded:</b> {this.getDateString(survey.lastResponded)}</p>
+								</p>
 
-							<div className="card-action">
-								<a>Yes: {survey.yes}</a>
-								<a>No: {survey.no}</a>
+								<div className="card-action">
+									<a>Yes: {survey.yes}</a>
+									<a>No: {survey.no}</a>
+								</div>
 							</div>
 						</div>
-					</div>
-				);
+					);
+				}
+				else {
+					return <div className="landing-title"><h3 className="landing-text-style">You have no surveys</h3></div>;
+				}
 			});
 		}
 		else {
